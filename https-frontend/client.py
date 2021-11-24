3ace0e21633b80eb8111ea48b93480a5128781cf
Jeff Hollan <jeffhollan@outlook.com>
2021-11-24 12:03:29 -0800

    flask working

--- https-frontend/client.py
+++ https-frontend/client.py
@@ -4,17 +4,27 @@ import grpc
 from protos import greet_pb2
 from protos import greet_pb2_grpc
 
-# open a gRPC channel
+import os
+import logging
+import flask
+from flask import request, jsonify
+from flask import json
+from flask_cors import CORS
+
+logging.basicConfig(level=logging.INFO)
+
+app = flask.Flask(__name__)
+CORS(app)
+
+# initialize the gRPC channel
 channel = grpc.insecure_channel('localhost:50051')
+greeter = greet_pb2_grpc.GreeterStub(channel)
 
-# create a stub (client)
-stub = greet_pb2_grpc.GreeterStub(channel)
+@app.route('/hello', methods=['GET'])
+def getOrder():
+    app.logger.info('order service called')
+    helloRequest = greet_pb2.HelloRequest(name='Azure Container Apps')
+    response = greeter.SayHello(helloRequest)
+    return response.message
 
-# create a valid request message
-helloRequest = greet_pb2.HelloRequest(name='Azure Container Apps')
-
-# make the call
-response = stub.SayHello(helloRequest)
-
-# et voilà
-print(response.message)
+app.run(host='0.0.0.0', port=os.getenv('PORT', '8050'))
